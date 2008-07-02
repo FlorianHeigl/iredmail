e4b0130d70b12db107043df75005224135e65472
michaelbibby <michaelbibby@localhost>
2008-07-02 17:54:31 +0800

    [svn r60] [iRedMail] Sync convert tool.

--- iRedMail/tools/convert_winmail_mailbox.sh
+++ iRedMail/tools/convert_winmail_mailbox.sh
@@ -9,37 +9,42 @@
 
 # Usage:
 
+# Your original WinMail user mailboxes.
+source_dir='./'
+# Copy to another directory, converted as standard IMAP directory structure.
 target_dir='./'
 
 for i in $(ls -d *@w-ibeda.com)
 do
-	username="$(echo $i | awk -F'@' '{print $1}')"
-	domain="$(echo $i | awk -F'@' '{print $2}')"
-	mailbox="$domain/$username/"
-	
-	#mailbox="$(echo $i | awk -F'@' '{print $1"/"$2"/"}')"
+    username="$(echo $i | awk -F'@' '{print $1}')"
+    domain="$(echo $i | awk -F'@' '{print $2}')"
+    mailbox="${target_dir}/$domain/$username/"
+    
+    #mailbox="$(echo $i | awk -F'@' '{print $1"/"$2"/"}')"
 
-	# Create necessary directories as mailbox format.
-	# Inbox.
-	mkdir -p ${mailbox}/{cur,new,tmp}
-	# Sent,Junk,Drafts,Trash
-	mkdir -p ${mailbox}/.{Sent,Junk,Drafts,Trash}/{cur,new,tmp}
+    # Create necessary directories as mailbox format.
+    # Inbox.
+    mkdir -p ${mailbox}/{cur,new,tmp}
+    # Sent,Junk,Drafts,Trash
+    mkdir -p ${mailbox}/.{Sent,Junk,Drafts,Trash}/{cur,new,tmp}
 
-	# Copy inbox.
-	for email in $(find $i -iname '*.in')
-	do
-		cp -f $email ${mailbox}/cur/
-	done
+    find_dir="${source_dir}/$i/"
 
-	# Copy deleted mails.
-	for email in $(find $i -iname '*.del')
-	do
-		cp -f $email ${mailbox}/.Trash/
-	done
+    # Copy inbox.
+    for email in $(find ${find_dir} -iname '*.in')
+    do
+        cp -f $email ${mailbox}/cur/
+    done
 
-	# Copy sent mails.
-	for email in $(find $i -iname '*.out')
-	do
-		cp -f $email ${mailbox}/.Sent/
-	done
+    # Copy deleted mails.
+    for email in $(find ${find_dir} -iname '*.del')
+    do
+        cp -f $email ${mailbox}/.Trash/
+    done
+
+    # Copy sent mails.
+    for email in $(find ${find_dir} -iname '*.out')
+    do
+        cp -f $email ${mailbox}/.Sent/
+    done
 done
