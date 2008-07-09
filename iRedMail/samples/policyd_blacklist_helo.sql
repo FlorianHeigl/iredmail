ecb37d9897ed11be0a1e26c7129a1a43b9fc199c
michaelbibby <michaelbibby@localhost>
2008-07-09 22:54:01 +0800

    [svn r111] [iRedMail]     * Add policyd blacklist_helo list. Thanks muniao. :)

---
+++ iRedMail/samples/policyd_blacklist_helo.sql
@@ -0,0 +1,17 @@
+/*
+This is the real HELO:
+    sohu.com    websmtp.sohu.com relay2nd.mail.sohu.com
+    126.com     m15-78.126.com
+    163.com     m31-189.vip.163.com m13-49.163.com
+    sina.com    mail2-209.sinamail.sina.com.cn
+    gmail.com   hu-out-0506.google.com
+*/
+
+USE policyd;
+
+INSERT INTO blacklist_helo (_helo) VALUES ("sina.com");
+INSERT INTO blacklist_helo (_helo) VALUES ("gmail.com");
+INSERT INTO blacklist_helo (_helo) VALUES ("google.com");
+INSERT INTO blacklist_helo (_helo) VALUES ("126.com");
+INSERT INTO blacklist_helo (_helo) VALUES ("163.com");
+INSERT INTO blacklist_helo (_helo) VALUES ("sohu.com");
