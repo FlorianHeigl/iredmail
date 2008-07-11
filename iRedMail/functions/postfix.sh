c8fb5d03b946e0765aa78bcb69911e5cb9f0bb48
michaelbibby <michaelbibby@localhost>
2008-07-11 13:03:25 +0800

    [svn r124] [iRedMail] Sync.

--- iRedMail/functions/postfix.sh
+++ iRedMail/functions/postfix.sh
@@ -576,7 +576,7 @@ postfix_config_sasl()
         #
         # Policyd, perl-Mail-SPF and non-SPF.
         #
-        postconf -e smtpd_recipient_restrictions="permit_mynetworks, reject_non_fqdn_sender_domain, reject_invalid_hostname, reject_unknown_sender_domain, reject_unknown_recipient_domain, reject_non_fqdn_sender, reject_non_fqdn_recipient, permit_sasl_authenticated, reject_unauth_destination, check_policy_service inet:127.0.0.1:10031"
+        postconf -e smtpd_recipient_restrictions="permit_mynetworks, reject_invalid_hostname, reject_unknown_sender_domain, reject_unknown_recipient_domain, reject_non_fqdn_sender, reject_non_fqdn_recipient, permit_sasl_authenticated, reject_unauth_destination, check_policy_service inet:127.0.0.1:10031"
     else
         :
     fi
