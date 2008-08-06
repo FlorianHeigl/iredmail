049937412983bbeea11722dabcd58df4f6e7b630
michaelbibby <michaelbibby@localhost>
2008-08-06 14:40:24 +0800

    [svn r269] [Doc] Sync.

--- tutorial/xml/compile.sh
+++ tutorial/xml/compile.sh
@@ -12,6 +12,7 @@ export UPDATE_TIME="$(/bin/date '+%Y.%m.%d %H:%M')"
 perl -pi -e 's#(.*Lastest update time: )(.*)(\..*)#${1}$ENV{UPDATE_TIME}${3}#' bookinfo.xml
 
 for flavor in 'mysql' 'openldap'; do
+    echo "Build flavor: ${flavor}."
     xsltproc \
         --stringparam html.stylesheet 'docbook.css' \
         --stringparam admon.graphics 1 \
