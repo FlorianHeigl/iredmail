72d4199bdb1c19ac8ad2990b3f1e3ff76f698733
michaelbibby <michaelbibby@localhost>
2008-08-06 13:42:07 +0800

    [svn r267] [Doc] Split to -mysql, -openldap flavors.

--- tutorial/xml/compile.sh
+++ tutorial/xml/compile.sh
@@ -11,17 +11,18 @@
 export UPDATE_TIME="$(/bin/date '+%Y.%m.%d %H:%M')"
 perl -pi -e 's#(.*Lastest update time: )(.*)(\..*)#${1}$ENV{UPDATE_TIME}${3}#' bookinfo.xml
 
-xsltproc \
-    --stringparam html.stylesheet 'docbook.css' \
-    --stringparam admon.graphics 1 \
-    --stringparam admon.graphics.extension '.png' \
-    --stringparam admon.graphics.path 'images/' \
-    --stringparam section.autolabel 1 \
-    --stringparam section.label.includes.component.label 1 \
-    --stringparam toc.section.depth 8 \
-    /usr/share/sgml/docbook/xsl-stylesheets-1.69.1-5.1/html/chunk.xsl \
-    iRedMail.xml 
-
+for flavor in 'mysql' 'openldap'; do
+    xsltproc \
+        --stringparam html.stylesheet 'docbook.css' \
+        --stringparam admon.graphics 1 \
+        --stringparam admon.graphics.extension '.png' \
+        --stringparam admon.graphics.path 'images/' \
+        --stringparam section.autolabel 1 \
+        --stringparam section.label.includes.component.label 1 \
+        --stringparam toc.section.depth 8 \
+        /usr/share/sgml/docbook/xsl-stylesheets-1.69.1-5.1/html/chunk.xsl \
+        iRedMail-doc-${flavor}.xml 
+done
 
 # Browse tutorial directly from '/svn/trunk/tutorial/':
 #svn propset svn:mime-type 'text/html' *html
