b5eb7e4cf339fa4853a71d5c3c28406d6184bd99
michaelbibby <michaelbibby@localhost>
2008-08-22 18:59:21 +0800

    [svn r319] [iRedMail] Fix incorrect home directory permission.

--- iRedMail/functions/user_vmail.sh
+++ iRedMail/functions/user_vmail.sh
@@ -8,6 +8,15 @@ adduser_vmail()
 
     ECHO_INFO "Add user/group: vmail."
     groupadd -g ${VMAIL_USER_GID} ${VMAIL_GROUP_NAME}
+
+    # Set permission for exist home directory.
+    if [ -d ${VMAIL_USER_HOME_DIR} ]; then
+        chown -R ${VMAIL_USER_NAME}:${VMAIL_GROUP_NAME} ${VMAIL_USER_HOME_DIR}
+        chmod -R 0700 ${VMAIL_USER_HOME_DIR}
+    else
+        :
+    fi
+
     useradd -m -d ${VMAIL_USER_HOME_DIR} -s /sbin/nologin -u ${VMAIL_USER_UID} -g ${VMAIL_USER_GID} ${VMAIL_USER_NAME}
     rm -f ${VMAIL_USER_HOME_DIR}/.* 2>/dev/null
 
