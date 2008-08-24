f0634d7468b54cd43818c957acc8865032e26011
michaelbibby <michaelbibby@localhost>
2008-08-24 11:42:19 +0800

    [svn r338] [iRedMail] Fix small typo error: tools/get_helo_count.sh.

--- iRedMail/tools/get_helo_count.sh
+++ iRedMail/tools/get_helo_count.sh
@@ -6,6 +6,6 @@
 export LC_ALL=C
 
 grep 'helo=' /var/log/maillog | awk -F'helo=<' '{print $2}' | awk -F'>' '{print $1}' > helo.all.iredmail
-sort /tmp/helo.all | uniq -c | sort -nr > helo.count.iredmail
+sort helo.all.iredmail | uniq -c | sort -nr > helo.count.iredmail
 rm -f helo.all.iredmail
 
