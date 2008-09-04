6f0d9abdd43eab3807ecf406637bb111ad6c49c2
michaelbibby <michaelbibby@localhost>
2008-09-04 23:05:53 +0800

    [svn r385] Remove init version of iRedAdmin. No maintainer(s).

--- iRedAdmin/includes/db_conn.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$link = mysql_connect('localhost',$config['dbuser'] , $config['dbpass']) or die('Could not connect: ' . mysql_error());
-mysql_select_db($config['dbname']) or die('Could not select database');
-
-?>
