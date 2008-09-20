732257dcea2358d928ce7eccfb96df7c91d9c6f2
michaelbibby <michaelbibby@localhost>
2008-09-20 10:24:54 +0800

    [svn r437] [iRedMail]     * Hide Apache + PHP version.

--- iRedMail/functions/apache_php.sh
+++ iRedMail/functions/apache_php.sh
@@ -6,7 +6,14 @@ apache_php_config()
 {
     backup_file ${HTTPD_CONF}
 
-    ECHO_INFO "Disable 'AddDefaultCharset' in Apache."
+    # --------------------------
+    # Apache Setting.
+    # --------------------------
+    ECHO_INFO "Hide apache software version: ${HTTPD_CONF}."
+    perl -pi -e 's#^(ServerTokens).*#${1} ProductOnly#' ${HTTPD_CONF}
+    perl -pi -e 's#^(ServerSignature).*#${1} EMail#' ${HTTPD_CONF}
+
+    ECHO_INFO "Disable 'AddDefaultCharset' in Apache: ${HTTPD_CONF}."
     perl -pi -e 's/^(AddDefaultCharset UTF-8)/#${1}/' ${HTTPD_CONF}
 
     if [ X"${HTTPD_PORT}" != X"80" ]; then
@@ -16,12 +23,18 @@ apache_php_config()
         :
     fi
 
+    # --------------------------
+    # PHP Setting.
+    # --------------------------
     backup_file ${PHP_INI}
 
-    ECHO_INFO "Increase 'memory_limit' to 128M in ${PHP_INI}."
-    perl -pi -e 's/^(memory_limit = )/${1}128M ;/' ${PHP_INI}
+    ECHO_INFO "Hide PHP Version in Apache from remote users requests: ${PHP_INI}."
+    perl -pi -e 's#^(expose_php.*=)#${1} Off;#' ${PHP_INI}
 
-    ECHO_INFO "Increase 'upload_max_filesize', 'post_max_size' to 10/12M in ${PHP_INI}."
+    ECHO_INFO "Increase 'memory_limit' to 128M: ${PHP_INI}."
+    perl -pi -e 's#^(memory_limit = )#${1} 128M;#' ${PHP_INI}
+
+    ECHO_INFO "Increase 'upload_max_filesize', 'post_max_size' to 10/12M: ${PHP_INI}."
     perl -pi -e 's/^(upload_max_filesize.*=)/${1}10M; #/' ${PHP_INI}
     perl -pi -e 's/^(post_max_size.*=)/${1}12M; #/' ${PHP_INI}
 
