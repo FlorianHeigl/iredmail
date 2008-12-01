013ef8429953a325a693ee78136c4ecc930723bc
michaelbibby <michaelbibby@localhost>
2008-12-01 10:32:57 +0800

    [svn r604] [iRedAdmin] Use mako template engine, with i18n support.

--- iRedAdmin/iredadmin.py
+++ iRedAdmin/iredadmin.py
@@ -6,9 +6,10 @@
 import os
 import sys
 import gettext
-
-# webpy.
 import web
+
+# Use Mako template engine.
+from web.contrib.template import render_mako
 
 urls = (
     '/.*', 'hello',
@@ -28,12 +29,25 @@ for libdir in ['libs', 'config']:
 import iredconf
 
 # i18n support in Python script and webpy template file.
+# TODO: per-user language setting.
 gettext.install('iredadmin', localedir, unicode=True)   
 gettext.translation('iredadmin', localedir, languages=[iredconf.LANG]).install(True)
 
+# Directory to be used as the Python egg cache directory.
+# Note that the directory specified must exist and be writable by the
+# user that the daemon process run as. 
 os.environ['PYTHON_EGG_CACHE'] = '/tmp/.iredadmin-eggs'
 
-render = web.template.render(curdir + '/templates/' + iredconf.SKIN, globals={'_': _})
+# input_encoding and output_encoding is important for unicode
+# template file. Reference:
+# http://www.makotemplates.org/docs/documentation.html#unicode
+# TODO: per-user skin setting.
+render = render_mako(
+        directories=[curdir + '/templates/' + iredconf.SKIN],
+        input_encoding='utf-8',
+        output_encoding='utf-8',
+        )
+
 
 class hello:
     def GET(self):
