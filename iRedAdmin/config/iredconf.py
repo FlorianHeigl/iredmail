bad2c98e86257d38b6c0f8c151518a393d7ce726
michaelbibby <michaelbibby@localhost>
2008-12-05 23:52:15 +0800

    [svn r637] [iRedAdmin] Sync.

--- iRedAdmin/config/iredconf.py
+++ iRedAdmin/config/iredconf.py
@@ -7,3 +7,15 @@ PROG = 'iRedAdmin'
 
 SKIN = 'default'
 LANG = 'zh_CN'
+
+
+# MySQL configure.
+DB_SERVER_ADDR = 'localhost'
+DB_SERVER_PORT = 3306
+
+# Session relate config.
+SESSION_DB_DBN = 'mysql'
+SESSION_DB_NAME = 'vmail'
+SESSION_DB_USER = 'vmailadmin'
+SESSION_DB_PASSWD = 'passwd'
+SESSION_DB_TABLE_SESSION = 'sessions'
--- iRedAdmin/iredadmin.py
+++ iRedAdmin/iredadmin.py
@@ -7,23 +7,33 @@ import os
 import sys
 import gettext
 import web
-
-# Use Mako template engine.
-from web.contrib.template import render_mako
+from web import form
 
 urls = (
-    '/.*', 'hello',
-    )
+        # Make url ending with or without '/' going to the same class.
+        "/(.*)/", "redirect",
+
+        '/login', 'login',
+        '/logout', 'logout',
+        )
+
+app = web.application(urls, globals())
+
+# Currently, sessions doesn't work in debug mode.
+web.config.debug = False
+
+# Internal error.
+#app.internalerror = web.debugerror
 
 # File location directory.
-curdir = os.path.dirname(__file__)
+rootdir = os.path.dirname(__file__)
 
 # i18n directory.
-localedir = curdir + '/i18n'
+localedir = rootdir + '/i18n'
 
 # Append current file localtion to sys path.
 for libdir in ['libs', 'config']:
-    sys.path.append(curdir + '/' + libdir)
+    sys.path.append(rootdir + '/' + libdir)
 
 # Import iRedAdmin config file.
 import iredconf
@@ -38,25 +48,69 @@ gettext.translation('iredadmin', localedir, languages=[iredconf.LANG]).install(T
 # user that the daemon process run as. 
 os.environ['PYTHON_EGG_CACHE'] = '/tmp/.iredadmin-eggs'
 
-# input_encoding and output_encoding is important for unicode
-# template file. Reference:
-# http://www.makotemplates.org/docs/documentation.html#unicode
-# TODO: per-user skin setting.
-render = render_mako(
-        directories=[curdir + '/templates/' + iredconf.SKIN],
-        input_encoding='utf-8',
-        output_encoding='utf-8',
+# Use webpy templator.
+render = web.template.render(rootdir + '/templates/' + iredconf.SKIN, globals={'_':_})
+
+# Customize page not found error.
+def notfound():
+    return web.notfound("Sorry, the page you were looking for was not found.")
+
+app.notfound = notfound
+
+# Sessions.
+sessionDB = web.database(
+        host = iredconf.DB_SERVER_ADDR,
+        port = int(iredconf.DB_SERVER_PORT),
+        dbn = iredconf.SESSION_DB_DBN,
+        db = iredconf.SESSION_DB_NAME,
+        user = iredconf.SESSION_DB_USER,
+        pw = iredconf.SESSION_DB_PASSWD,
         )
 
+if web.config.get('_session') is None:
+    sessionStore = web.session.DBStore(sessionDB, iredconf.SESSION_DB_TABLE_SESSION)
+    # Initialize.
+    session = web.session.Session(app, sessionStore, initializer={'username': 'Anonymous'})
+    web.config._session = session
+else:
+    session = web.config._session
 
-class hello:
+class redirect:
+    '''Make url ending with or without '/' going to the same class.
+    '''
+    def GET(self, path):
+        web.seeother('/' + path)
+
+def logged(session):
+    if "logged" in session:
+        if session["logged"] == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+class login:
+    # Login form
+    vusername = form.regexp(r".*@.*", _('Must be a valid email address'))
+    login_form = form.Form(
+            form.Textbox('username', vusername, description=_('Username:')),
+            form.Password('password', description=_('Password:')),
+            form.Button("submit", type="submit", description=_('Login')),
+            )
+
     def GET(self):
-        return render.index(name=_("Message"))
+        # Show login page.
+        f = self.login_form()
+        return render.login(form=f)
 
-# Run with buildin http server.
-#app = web.application(urls, globals())
-#if __name__ == "__main__":
-#    app.run()
+    def POST(self):
+        f = self.login_form()
+        if not f.validates():
+            return render.login(f)
+        else:
+            web.seeother('./login')
+
 
 # Run with Apache + mod_wsgi.
-application = web.application(urls, globals()).wsgifunc()
+application = app.wsgifunc() 
