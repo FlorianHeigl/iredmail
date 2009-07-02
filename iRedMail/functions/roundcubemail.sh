5eb4da6838131d6f5c0ebfae61d5e898c35c709f
Zhang Huangbin <Zhang Huangbin@localhost>
2009-07-02 14:33:57 +0800

    [iRedMail]     * Improve global ldap address book in roundcube, make it search only users       in the same domain.

--- iRedMail/functions/roundcubemail.sh
+++ iRedMail/functions/roundcubemail.sh
@@ -185,12 +185,17 @@ EOF
     'hosts'         => array("${LDAP_SERVER_HOST}"),
     'port'          => ${LDAP_SERVER_PORT},
     'use_tls'       => false,
-    //'user_specific' => true, // If true the base_dn, bind_dn and bind_pass default to the user's IMAP login.
-    //'base_dn'       => "${LDAP_ATTR_DOMAIN_RDN}=%d,${LDAP_BASEDN}",
-    //'base_dn'       => "${LDAP_ATTR_GROUP_RDN}=${LDAP_ATTR_GROUP_USERS},${LDAP_ATTR_DOMAIN_RDN}=${FIRST_DOMAIN},${LDAP_BASEDN}",
-    'base_dn'       => "${LDAP_ATTR_DOMAIN_RDN}=${FIRST_DOMAIN},${LDAP_BASEDN}",
-    'bind_dn'       => "${LDAP_BINDDN}",
-    'bind_pass'     => "${LDAP_BINDPW}",
+
+    // ---- Used to search accounts only in the same domain. ----
+    'user_specific' => true, // If true the base_dn, bind_dn and bind_pass default to the user's IMAP login.
+    'base_dn'       => "${LDAP_ATTR_DOMAIN_RDN}=%d,${LDAP_BASEDN}",
+    'bind_dn'       => "${LDAP_ATTR_USER_RDN}=%u@%d,${LDAP_ATTR_GROUP_RDN}=${LDAP_ATTR_GROUP_USERS},${LDAP_ATTR_DOMAIN_RDN}=%d,${LDAP_BASEDN}",
+
+    // ---- Uncomment below lines to search whole LDAP tree ----
+    //'base_dn'       => "${LDAP_BASEDN}",
+    //'bind_dn'       => "${LDAP_BINDDN}",
+    //'bind_pass'     => "${LDAP_BINDPW}",
+
     'writable'      => false, // Indicates if we can write to the LDAP directory or not.
     // If writable is true then these fields need to be populated:
     // LDAP_Object_Classes, required_fields, LDAP_rdn
