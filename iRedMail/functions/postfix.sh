50fd88a69de346ebc6484b01725861dfa3be5e5f
Zhang Huangbin <Zhang Huangbin@localhost>
2009-08-19 22:20:55 +0800

    Add missing service control in postfix ldap lookup table.

--- iRedMail/functions/postfix.sh
+++ iRedMail/functions/postfix.sh
@@ -294,7 +294,7 @@ bind_dn         = ${LDAP_BINDDN}
 bind_pw         = ${LDAP_BINDPW}
 search_base     = ${ldap_search_base_user}
 scope           = one
-query_filter    = (&(objectClass=${LDAP_OBJECTCLASS_MAILUSER})(${LDAP_ATTR_USER_RDN}=%s)(${LDAP_ATTR_ACCOUNT_STATUS}=${LDAP_STATUS_ACTIVE})(${LDAP_ENABLED_SERVICE}=${LDAP_SERVICE_MAIL}))
+query_filter    = (&(objectClass=${LDAP_OBJECTCLASS_MAILUSER})(${LDAP_ATTR_USER_RDN}=%s)(${LDAP_ATTR_ACCOUNT_STATUS}=${LDAP_STATUS_ACTIVE})(${LDAP_ENABLED_SERVICE}=${LDAP_SERVICE_MAIL})(${LDAP_ENABLED_SERVICE}=${LDAP_SERVICE_DELIVER}))
 result_attribute= mailMessageStore
 debuglevel      = 0
 EOF
