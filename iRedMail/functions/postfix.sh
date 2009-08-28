d5dd2e197d60cd2ffbe5577c83152f8068790c1c
Zhang Huangbin <Zhang Huangbin@localhost>
2009-08-28 13:39:03 +0800

    Re-enable sender_login_mismatch in main.cf.

--- iRedMail/functions/postfix.sh
+++ iRedMail/functions/postfix.sh
@@ -53,8 +53,7 @@ EOF
     postconf -e receive_override_options='no_address_mappings'
     postconf -e smtpd_data_restrictions='reject_unauth_pipelining'
     postconf -e smtpd_reject_unlisted_recipient='yes'   # Default
-    #postconf -e smtpd_sender_restrictions="permit_mynetworks, reject_sender_login_mismatch, permit_sasl_authenticated"
-    postconf -e smtpd_sender_restrictions="permit_mynetworks, permit_sasl_authenticated"
+    postconf -e smtpd_sender_restrictions="permit_mynetworks, reject_sender_login_mismatch, permit_sasl_authenticated"
     postconf -e delay_warning_time='0h'
     postconf -e policy_time_limit='3600'
     postconf -e maximal_queue_lifetime='1d'
