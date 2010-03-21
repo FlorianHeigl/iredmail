4819b30b504a0713bb6c72200ff183211eed1552
Zhang Huangbin <Zhang Huangbin@localhost>
2010-03-21 10:52:48 +0800

    Don't install mailx on Debian/Ubuntu.

--- iRedMail/functions/packages.sh
+++ iRedMail/functions/packages.sh
@@ -225,7 +225,7 @@ install_all()
         ALL_PKGS="${ALL_PKGS} bzip2.${ARCH} acl.${ARCH} mailx.${ARCH} patch.${ARCH} vixie-cron.${ARCH} tmpwatch.${ARCH} crontabs.noarch dos2unix.${ARCH}"
         ENABLED_SERVICES="${ENABLED_SERVICES} crond"
     elif [ X"${DISTRO}" == X"DEBIAN" -o X"${DISTRO}" == X"UBUNTU" ]; then
-        ALL_PKGS="${ALL_PKGS} bzip2 acl mailx patch cron tofrodos"
+        ALL_PKGS="${ALL_PKGS} bzip2 acl patch cron tofrodos"
         ENABLED_SERVICES="${ENABLED_SERVICES} cron"
     else
         :
