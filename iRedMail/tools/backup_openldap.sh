989aef9e86b1f745633c103f9d915109ad91a178
Zhang Huangbin <Zhang Huangbin@localhost>
2010-09-24 13:41:02 +0800

    Add script used to backup OpenLDAP data: tools/backup_openldap.sh.

--- iRedMail/tools/backup_openldap.sh
+++ iRedMail/tools/backup_openldap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Author:   Zhang Huangbin (zhb@iredmail.org)
-# Date:     16/09/2007
-# Purpose:  Backup specified mysql databases with command 'mysqldump'.
+# Date:     05/02/2010
+# Purpose:  Dump whole LDAP tree with command 'slapcat'.
 # License:  This shell script is part of iRedMail project, released under
 #           GPL v2.
 
@@ -11,7 +11,7 @@
 ###########################
 #
 #   * Required commands:
-#       + mysqldump
+#       + slapcat
 #       + du
 #       + bzip2 or gzip     # If bzip2 is not available, change 'CMD_COMPRESS'
 #                           # to use 'gzip'.
@@ -27,17 +27,13 @@
 #   * Set correct values for below variables:
 #
 #       BACKUP_ROOTDIR
-#       MYSQL_USER
-#       MYSQL_PASSWD
-#       DATABASES
-#       DB_CHARACTER_SET
 #       COMPRESS
 #       DELETE_PLAIN_SQL_FILE
 #
 #   * Add crontab job for root user (or whatever user you want):
 #
 #       # crontab -e -u root
-#       1   4   *   *   *   bash /path/to/backup_mysql.sh
+#       1   4   *   *   *   bash /path/to/backup_openldap.sh
 #   
 #   * Make sure 'crond' service is running, and will start automatically when
 #     system startup:
@@ -56,46 +52,35 @@
 ###########################
 #
 #   $BACKUP_ROOTDIR             # Default is /backup
-#       |- mysql/               # Used to store all backed up databases.
+#       |- ldap/                # Used to store all backed up copies.
 #           |- YEAR.MONTH/
 #               |- YEAR.MONTH.DAY/
-#                   |- DB.YEAR.MONTH.DAY.MIN.HOUR.SECOND.sql
-#                               # Backup copy, plain SQL file.
+#                   |- YEAR.MONTH.DAY.MIN.HOUR.SECOND.ldif
+#                               # Backup copy, plain LDIF file.
 #                               # Note: it will be removed immediately after
 #                               # it was compressed with success and 
 #                               # DELETE_PLAIN_SQL_FILE='YES'
 #
-#                   |- DB.YEAR.MONTH.DAY.HOUR.MINUTE.SECOND.sql.bz2
-#                               # Backup copy, compressed SQL file.
+#                   |- YEAR.MONTH.DAY.HOUR.MINUTE.SECOND.ldif.bz2
+#                               # Backup copy, compressed LDIF file.
 #
 #       |- logs/
 #           |- YEAR.MONTH/
-#               |- mysql-YEAR.MONTH.DAY.MIN.HOUR.SECOND.log     # Log file
+#               |- ldap-YEAR.MONTH.DAY.MIN.HOUR.SECOND.log     # Log file
 #
 
 #########################################################
 # Modify below variables to fit your need ----
 #########################################################
+
 # Where to store backup copies.
 BACKUP_ROOTDIR='/backup'
-
-# MySQL user and password.
-MYSQL_USER='root'
-MYSQL_PASSWD='root_password'
-
-# Which database(s) we should backup. Multiple databases MUST be seperated by
-# a SPACE.
-DATABASES="mysql vmail policyd roundcubemail"
-
-# Database character set for ALL databases.
-# Note: Currently, it doesn't support to specify character set for each databases.
-DB_CHARACTER_SET="utf8"
 
 # Compress plain SQL file: YES, NO.
 COMPRESS="YES"
 
-# Delete plain SQL files after compressed. Compressed copy will be remained.
-DELETE_PLAIN_SQL_FILE="YES"
+# Delete plain LDIF files after compressed. Compressed copy will be remained.
+DELETE_PLAIN_LDIF_FILE="YES"
 
 #########################################################
 # You do *NOT* need to modify below lines.
@@ -104,70 +89,61 @@ DELETE_PLAIN_SQL_FILE="YES"
 CMD_DATE='/bin/date'
 CMD_DU='du -sh'
 CMD_COMPRESS='bzip2 -9'
-CMD_MYSQLDUMP='mysqldump'
+CMD_SLAPCAT='slapcat'
 
 # Date.
 MONTH="$(${CMD_DATE} +%Y.%m)"
 DAY="$(${CMD_DATE} +%Y.%m.%d)"
 DATE="$(${CMD_DATE} +%Y.%m.%d.%H.%M.%S)"
 
+#########
 # Define, check, create directories.
-BACKUP_DIR="${BACKUP_ROOTDIR}/mysql/${MONTH}/${DAY}"
+#
+# Backup directory.
+BACKUP_DIR="${BACKUP_ROOTDIR}/ldap/${MONTH}/${DAY}"
+[ -d ${BACKUP_DIR} ] || mkdir -p ${BACKUP_DIR}
 
-# Check and create directories.
-[ -d ${BACKUP_DIR} ] || mkdir -p ${BACKUP_DIR} 2>/dev/null
+# Backup file name.
+BACKUP_FILE="${BACKUP_DIR}/${DATE}.ldif"
 
 # Logfile directory. Default is /backup/logs/YYYY.MM/.
 LOG_DIR="${BACKUP_ROOTDIR}/logs/${MONTH}/"
 [ -d ${LOG_DIR} ] || mkdir -p ${LOG_DIR} 2>/dev/null
 
 # Log file. Default is /backup/logs/YYYY.MM/mysql-YYYY.MM.DD.log.
-LOGFILE="${LOG_DIR}/mysql-${DATE}.log"
+LOGFILE="${LOG_DIR}/ldap-${DATE}.log"
 
+############
 # Initialize log file.
+#
 echo "* Starting backup: ${DATE}." >${LOGFILE}
 echo "* Log file: ${LOGFILE}." >>${LOGFILE}
 echo "* Backup copies: ${BACKUP_DIR}." >>${LOGFILE}
 
-backup_db()
-{
-    # USAGE:
-    #  # backup dbname
-    output_sql="${BACKUP_DIR}/${1}.${DATE}.sql"
+##############
+# Backing up
+#
 
-    mysqldump \
-        -u${MYSQL_USER} \
-        -p${MYSQL_PASSWD} \
-        --default-character-set=${DB_CHARACTER_SET} \
-        $1 > ${output_sql}
-}
-
-# Backup.
-for db in ${DATABASES}
-do
-        echo -n "* Backing up database: ${db}..." >>${LOGFILE}
-        backup_db ${db} >>${LOGFILE} 2>&1
-        echo -e "\tDone" >>${LOGFILE}
-done
+echo "* Dumping LDAP data into file: ${BACKUP_FILE}..." >>${LOGFILE}
+${CMD_SLAPCAT} > ${BACKUP_FILE}
 
 # Compress plain SQL file.
 if [ X"${COMPRESS}" == X"YES" ]; then
-    for sql_file in $(ls ${BACKUP_DIR}/*); do
-        echo -n "* Compressing plain SQL file: ${sql_file}..." >>${LOGFILE}
-        ${CMD_COMPRESS} ${sql_file} >>${LOGFILE} 2>&1
+    echo -n "* Compressing LDIF file ..."
+    ${CMD_COMPRESS} ${BACKUP_FILE} >>${LOGFILE} 2>&1
 
-        if [ X"$?" == X"0" ]; then
-            echo -e "\tDone" >>${LOGFILE}
+    if [ X"$?" == X"0" ]; then
+        echo -e "\tDone" >>${LOGFILE}
 
-            # Delete plain SQL file after compressed.
-            if [ X"${DELETE_PLAIN_SQL_FILE}" == X"YES" -a -f ${sql_file} ]; then
-                echo -n "* Removing plain SQL file: ${sql_file}..." >>${LOGFILE}
-                rm -f ${BACKUP_DIR}/*sql >>${LOGFILE} 2>&1
-                [ X"$?" == X"0" ] && echo -e "\tDone" >>${LOGFILE}
-            fi
+        # Delete plain LDIF file after compressed.
+        if [ X"${DELETE_PLAIN_LDIF_FILE}" == X"YES" -a -f ${BACKUP_FILE} ]; then
+            echo -n "* Removing plain LDIF file: ${BACKUP_FILE}..." >>${LOGFILE}
+            rm -f ${BACKUP_DIR}/*.ldif >>${LOGFILE} 2>&1
+            [ X"$?" == X"0" ] && echo -e "\tDone" >>${LOGFILE}
         fi
-    done
+    fi
 fi
+
 
 # Append file size of backup files.
 echo "* File size:" >>${LOGFILE}
